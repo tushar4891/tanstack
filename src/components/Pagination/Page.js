9ee9dde5e5cda77d5f1cc3bbed41d766a4fdd227
Tushar Koshti <tushar@zettabolt.com>
2024-09-27 10:45:01 +0530

    pagination updated

--- src/components/Pagination/Page.js
+++ src/components/Pagination/Page.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 export const Page = ({ table }) => {
+  const rowsCount = table.getRowModel().rows.length; // Get the number of rows
+
+  // If there are no rows, do not render the pagination
+  if (rowsCount === 0) {
+    return null;
+  } else {
+    console.log("row count ", rowsCount);
+  }
+
   return (
     <div className="mt-4 mb-5 d-flex justify-content-center align-items-center gap-3">
       <button onClick={() => table.setPageIndex(0)}>First page</button>
